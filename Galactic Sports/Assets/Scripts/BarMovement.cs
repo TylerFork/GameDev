d9e3e711b2dbb404d390ee6b3851103dab6dbdf8
TylerFork <68755052+TylerFork@users.noreply.github.com>
2021-04-04 09:24:41 -0600

    Add files via upload

---
+++ Galactic Sports/Assets/Scripts/BarMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarMovement : MonoBehaviour
+{
+private Vector3 posA;
+private Vector3 posB;
+private Vector3 nextPos;
+public float speed;
+[SerializeField]
+private Transform childTransform;
+[SerializeField]
+private Transform transformB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posA = childTransform.localPosition;
+        posB = transformB.localPosition;
+        nextPos = posB;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        childTransform.localPosition = Vector3.MoveTowards(childTransform.localPosition, nextPos, speed * Time.deltaTime);
+    }
+}
